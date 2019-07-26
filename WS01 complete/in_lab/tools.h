d70f4282807f14da207fc67d4861cc6e6f423257
Fabio Martinez <fabio_bernalmartinez@outlook.com>
2019-07-25 21:58:47 -0400

    Add files via upload

---
+++ WS01 complete/in_lab/tools.h
@@ -0,0 +1,18 @@
+/*****************************************
+Name:Fabio Bernal
+Course:CPD
+Student ID: 036422103
+Professor: Peter Liu
+*****************************************/
+
+
+#ifndef NAMESPACE_TOOLS_H
+#define NAMESPACE_TOOLS_H
+namespace sict {
+
+
+	int getInt(int min, int max);
+
+	int menu();
+}
+#endif 
