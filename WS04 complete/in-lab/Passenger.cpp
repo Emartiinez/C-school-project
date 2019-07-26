8caa87da7f3c7c01b2a56a897955e6e356da9200
Fabio Martinez <fabio_bernalmartinez@outlook.com>
2019-07-25 22:12:24 -0400

    Add files via upload

---
+++ WS04 complete/in-lab/Passenger.cpp
@@ -0,0 +1,141 @@
+// TODO: add file header comments here
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+#include "Passenger.h"
+
+using namespace std;
+
+
+// TODO: add your headers here
+
+namespace sict
+{
+
+
+
+	Passenger::Passenger()
+	{
+		Passenger_Name[0] = '\0';
+		Destination[0] = '\0';
+		year = 0;
+		month = 0;
+		day = 0;
+	}
+
+
+	Passenger::Passenger(const char* passengerName, const char* p_Destination)
+	{
+		if (passengerName != '\0' && p_Destination != '\0')
+		{
+			strcpy(Passenger_Name, passengerName);
+			strcpy(Destination, p_Destination);
+		}
+
+		else
+		{
+			*this = Passenger();
+		}
+	}
+
+	Passenger::Passenger(const char* name, const char* Pdestination, int Pyear, int Pmonth, int Pday)
+	{
+		if (name != '\0' && Pdestination != '\0')
+		{
+			if (Pyear >= MIN_YEAR && Pyear <= MAX_YEAR && Pmonth >= MIN_MONTH && Pmonth <= MAX_MONTH && Pday >= MIN_DAY && Pday <= MAX_DAY)
+			{
+				strncpy(Passenger_Name, name, MAX_NAME);
+				strncpy(Destination, Pdestination, MAX_NAME);
+				year = Pyear;
+				month = Pmonth;
+				day = Pday;
+			}
+		}
+		else
+		{
+			*this = Passenger();
+
+		}
+
+	}
+
+	//END of CONSTRUCTORS
+
+
+
+	bool Passenger::isEmpty() const
+	{
+		bool ToF;
+
+		if (Passenger_Name[0] != '\0' && Destination[0] != '\0' && year<=MAX_YEAR && year>=MIN_YEAR)
+		{
+			ToF = false;
+		}
+		else
+		{
+			ToF = true;
+		}
+		return ToF;
+	}
+
+	void Passenger::display()const
+
+	{
+		if (!isEmpty())
+		{
+			cout << Passenger_Name << " - " << Destination << " on " << year << "/";
+			cout.width(2);
+			cout.fill('0');
+			cout << month << "/";
+			cout.width(2);
+			cout.fill('0');
+			cout << day << endl;
+
+		}
+		else
+		{
+			cout << "No Passenger!" << endl;
+
+		}
+	}
+
+	const char* Passenger::name()const
+	{
+			return Passenger_Name;
+		
+	}
+
+	bool Passenger::canTravelWith(const Passenger& passenger) const
+	{
+		bool ToF;
+
+		if (!strcmp(passenger.Destination, Destination) && passenger.year == year && passenger.month == month && passenger.day == day)
+		{
+			ToF = true;
+		}
+		else
+		{
+			ToF = false;
+		}
+		return ToF;
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
---
+++ WS04 complete/in-lab/Passenger.h
@@ -0,0 +1,48 @@
+// TODO: add file header comments here
+
+#define _CRT_SECURE_NO_WARNINGS
+#ifndef SICT_PASSENGER_H
+#define SICT_PASSENGER_H
+// TODO: add header file guard here
+namespace sict
+{
+
+	const int MAX_NAME = 32;
+
+	const int MAX_YEAR = 2020;
+	const int MIN_YEAR = 2017;
+
+	const int MAX_MONTH = 12;
+	const int MIN_MONTH = 1;
+
+	const int MAX_DAY = 31;
+	const int MIN_DAY = 1;
+
+
+	class Passenger
+	{
+		char Passenger_Name[MAX_NAME];
+		char Destination[MAX_NAME];
+		int year;
+		int month;
+		int day;
+
+
+	public:
+
+		Passenger();
+		Passenger(const char*, const char*);
+		Passenger(const char*, const char*, int, int, int);
+
+		bool isEmpty() const;
+		void display() const;
+		const char* name()const;
+		bool canTravelWith(const Passenger&) const;
+
+	};
+
+
+}
+
+
+#endif 
